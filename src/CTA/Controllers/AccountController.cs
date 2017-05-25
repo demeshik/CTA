0e03fee2ae811fed0bc4919a7bfb662bcc50cc89
djrazor308 <djrazor308@hotmail.com>
2017-05-25 23:17:49 +0300

    Account view was added

---
+++ src/CTA/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using CTA.Models;
+using Microsoft.AspNetCore.Authorization;
+using AutoMapper;
+using CTA.DTO;
+
+namespace CTA.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> userManager;
+        public AccountController(UserManager<User> _userManager)
+        {
+            userManager = _userManager;
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            var currentUser = userManager.FindByNameAsync(User.Identity.Name).Result;
+
+            Mapper.Initialize(cfg => cfg.CreateMap<User, UserDTO>());
+            return View(Mapper.Map<UserDTO>(currentUser));
+        }
+    }
+}
